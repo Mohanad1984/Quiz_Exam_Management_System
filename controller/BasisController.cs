c133472267b53808f78e7b8b112d4356c81e7fc2
Mohanad1984 <88400999+Mohanad1984@users.noreply.github.com>
2022-11-04 00:41:41 +0100

    Add files via upload

---
+++ controller/BasisController.cs
@@ -0,0 +1,55 @@
+﻿using Logic;
+using QuizDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controller
+{
+    public abstract class BasisController
+    {
+        internal DatenBank _DbContext;
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public BasisController(DatenBank pContext)
+        {
+            if (pContext is null)
+            {
+                throw new Exception("Kein Kontext-Factory übergeben!!");
+            }
+            this._DbContext = pContext;
+        }
+
+        public bool CreateEntity()
+        {
+            bool Result = false;
+            this._DbContext.UserTab.Id = 1;
+            return Result;
+        }
+
+        public bool Save()
+        {
+            return false;
+        }
+
+        public bool UpdateEntity()
+        {
+            return true;
+        }
+
+        public bool DeleteEntity(int Id = 0)
+        {
+            return true;
+        }
+
+        public string GetEntity(int Id = 0, bool AsTracking = false)
+        {
+            return "";
+        }
+
+        protected abstract void CreateDemoData();
+    }
+}
---
+++ controller/ErgebnisController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controller
+{
+    public class ErgebnisController : BasisController
+    {
+        protected override void CreateDemoData()
+        {
+            this.Save();
+        }
+    }
+}
